a97a86eeeb4129b442019db049b9c18956228bf6
Cecil <saysea07@gmail.com>
2025-06-17 13:10:12 +1000

    Remove unused endpoints

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\SampleController;
 use App\Http\Controllers\CatalogueController;
 
 Route::get('/catalogues', [CatalogueController::class, 'index']);
 
-Route::get('/api-integration',[SampleController::class,'index']);
